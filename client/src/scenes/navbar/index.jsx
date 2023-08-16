2ebd2cf4441eb46273f3605c3d21125c82067835
notbil <notbilx@gmail.com>
2023-08-16 09:43:25 -0500

    login and authorization now works

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Discourse
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
